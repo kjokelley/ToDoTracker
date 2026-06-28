638224eb4838c06b92a029d88e580c25922b780d
kyle o <kyle@caspian.caspian>
2026-06-28 11:41:39 -0700

    Tried to remove .git in subfolder

--- python_db/Printer_Control/print_task.py
+++ python_db/Printer_Control/print_task.py
@@ -2,7 +2,7 @@ from escpos.printer import Usb
 import sys
 #from Python_DB import task_handler as th
 
-def print_task_by_id(task_id):
+def print_task_by_id(task_name):
     """ Seiko Epson Corp. Receipt Printer (EPSON TM-T88III) """
     p = Usb(0x04b8, 0x0202, 0, profile="TM-T88V")
     #p.text("Hello World\n")
@@ -12,7 +12,7 @@ def print_task_by_id(task_id):
     #p.barcode('4006381333931', 'EAN13', 64, 2, '', '')
 
     #task = th.get_task(task_id)
-    p.text("Hello\n")
+    p.text(task_name + "\n")
     #if len(sys.argv) != 4:
     #    raise ValueError("4 arguments required, only " + str(len(sys.argv)) + " were passed")
     #def print_task(task_name, task_type, priority):
@@ -23,4 +23,4 @@ def print_task_by_id(task_id):
     #p.text(task[2] + "\n")
     #p.text(str(task[4]) + "\n")
 
-print_task_by_id(1)
+
--- python_db/main.py
+++ python_db/main.py
@@ -3,7 +3,11 @@ import pathlib
 from Python_DB import task_handler as th
 from Printer_Control import print_task as pt
 
-th.create_task()
+#th.create_task()
+task_json = th.get_all_tasks()
+task = task_json[0][1]
+print(task)
+pt.print_task_by_id(task)
 #pt.print_task_by_id("1")
 #th.move_task("1")
 #th.create_task()
