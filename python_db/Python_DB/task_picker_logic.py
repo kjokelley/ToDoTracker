1418c7f27b345e4ac5c01d10381df655931c7b18
kjokelley <kjokelley@gmail.com>
2026-06-29 20:08:57 -0700

    Added active task logic to task picker

--- python_db/Python_DB/task_picker_logic.py
+++ python_db/Python_DB/task_picker_logic.py
@@ -8,36 +8,45 @@ import random
 #Task day (5,1) = 6, Task day (4,2) or (4, 1, 1) = 6, Task day (3, 1, 1, 1) = 6
 
 def pickTasks(tasks):
+    taskDayTotal = 0
+    activeTasks = []
     taskDict = {1:[], 2:[], 3:[], 4:[], 5:[]}
     runningTotalOfPriority = {1:0, 2:0, 3:0, 4:0, 5:0}
-
     for x in tasks:
+        if(x[8] == True):
+            if(x[7] == False):
+                activeTasks.append(x[0])
+                taskDayTotal += x[3]
+            else:
+                activeTasks.append(x[0])
+
         #taskDict[x[0]] = x[2]
-        match x[3]:
-            case 1:
-                runningTotalOfPriority[1] += 1
-                taskDict[1].append(x[0])
-            case 2:
-                runningTotalOfPriority[2] += 1
-                taskDict[2].append(x[0])
-            case 3:
-                runningTotalOfPriority[3] += 1
-                taskDict[3].append(x[0])
-            case 4:
-                runningTotalOfPriority[4] += 1
-                taskDict[4].append(x[0])
-            case 5:
-                runningTotalOfPriority[5] += 1
-                taskDict[5].append(x[0])
-            case _:
-                print("err: Task Priority not in list...")
+        if(x[7] == False):
+            match x[3]:
+                case 1:
+                    runningTotalOfPriority[1] += 1
+                    taskDict[1].append(x[0])
+                case 2:
+                    runningTotalOfPriority[2] += 1
+                    taskDict[2].append(x[0])
+                case 3:
+                    runningTotalOfPriority[3] += 1
+                    taskDict[3].append(x[0])
+                case 4:
+                    runningTotalOfPriority[4] += 1
+                    taskDict[4].append(x[0])
+                case 5:
+                    runningTotalOfPriority[5] += 1
+                    taskDict[5].append(x[0])
+                case _:
+                    print("err: Task Priority not in list...")
 
 
     print(taskDict)
     print(runningTotalOfPriority)
 
-    activeTasks = []
-    taskDayTotal = 0
+    
+    
     while taskDayTotal < 6:
         if(runningTotalOfPriority[5] >= 1 and (taskDayTotal + 5) <= 6):
             activeTasks.append(taskDict[5][random.randint(0, len(taskDict[5]) - 1)])
